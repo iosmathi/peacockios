4a88e0a8672d1e237eccdcf0f35d02b2be6f41e9
Mathivathani <mathi@torustech.co>
2018-08-30 12:04:38 +0530

    Initial branch commit

---
+++ Peacock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peacock
+//
+//  Created by Mathivathani Murugesan on 30/08/18.
+//  Copyright © 2018 Mathivathani Murugesan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
